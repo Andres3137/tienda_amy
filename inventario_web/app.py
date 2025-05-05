86331080f7c244697cb8434db2da6d51750c1a60
andres <andres06felipe20@gmail.com>
2025-05-04 21:17:29 -0500

    lista de productos y datos del producto

--- inventario_web/app.py
+++ inventario_web/app.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 import sqlite3
 
 app = Flask(__name__)
 
-DATABASE = '../tienda.db'  # Asegúrate de que esta ruta sea correcta
+DATABASE = '../tienda.db'  
 
 def get_db():
-    conn = sqlite3.connect(DATABASE)
-    conn.row_factory = sqlite3.Row
-    return conn
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+    return db
+
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route('/')
 def home():
@@ -22,6 +32,18 @@ def listar_productos():
     productos = cursor.fetchall()
     conn.close()
     return render_template('productos.html', productos=productos)
+
+@app.route('/producto/<int:id>')
+def detalle_producto(id):
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, codigo, nombre, cantidad, precio_venta, precio_proveedor FROM productos WHERE id = ?", (id,))
+    producto = cursor.fetchone()
+    conn.close()
+    if producto:
+        return render_template('detalle_producto.html', producto=producto)
+    else:
+        return render_template('error_producto.html', mensaje="producto no encontrado")
 
 if __name__ == "__main__":
     app.run(debug=True)
